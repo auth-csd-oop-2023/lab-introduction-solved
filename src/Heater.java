1bc8232943f93c15ea439996558d82793833c819
Athanasios Lagopoulos <sakrifor@gmail.com>
2018-10-05 11:34:07 +0300

    English Translation

--- src/Heater.java
+++ src/Heater.java
@@ -1,31 +1,42 @@
 /**
- * Η κλάση αυτή αφορά σε ένα θερμαντικό σώμα, το οποίο θα ξεκινάει στους 15 
- * βαθμούς κελσίου. Η θερμοκρασία του θα μπορεί να αυξηθεί ή να μειωθεί κατά 
- * ένα βήμα, το οποίο αρχικά θα είναι 5 βαθμοί κελσίου. Η ρύθμιση της 
- * θερμοκρασίας δεν θα πρέπει να βγαίνει εκτός ενός κατώτερου και ενός ανώτερου 
- * ορίου. 
- *
+ * Η κλάση αυτή αφορά σε ένα θερμαντικό σώμα, το οποίο θα ξεκινάει στους 15
+ * βαθμούς κελσίου. Η θερμοκρασία του θα μπορεί να αυξηθεί ή να μειωθεί κατά
+ * ένα βήμα, το οποίο αρχικά θα είναι 5 βαθμοί κελσίου. Η ρύθμιση της
+ * θερμοκρασίας δεν θα πρέπει να βγαίνει εκτός ενός κατώτερου και ενός ανώτερου
+ * ορίου.
+ * <p>
+ * This class represents a heater/radiator which has a starting temperature of 15
+ * degrees. Its temperature should be able to be increased or decreased using a step.
+ * Initially, this step is set to 5 degrees. The temperature should not exceed an upper
+ * and a lower limit.
  */
 public class Heater {    
     /* 
     O κατασκευαστής θα δέχεται δύο παραμέτρους. Η πρώτη 
     παράμετρος θα αφορά στην ελάχιστη και η δεύτερη στην μέγιστη θερμοκρασία 
-    στην οποία επιτρέπεται να ρυθμιστεί το θερμαντικό σώμα. 
+    στην οποία επιτρέπεται να ρυθμιστεί το θερμαντικό σώμα.
+
+    The constructor should have two parameters. The first is the minimum temperature
+    and the second parameter is the maximum temperature which the heater can be set to.
     */
 
     // Η μέθοδος αυτή θα αυξάνει την θερμοκρασία σύμφωνα με το βήμα
+    // This method increases the temperature using the step
     public void warmer() {
     }
 
     // Η μέθοδος αυτή θα μειώνει την θερμοκρασία σύμφωνα με το βήμα
+    // This method decreases the temperature using the step
     public void colder() {
     }
 
     //Η μέθοδος αυτή θα αλλάζει το βήμα
+    // This method changes the step
     public void setIncrement(double step) {
     }
 
     // η μέθοδος αυτή θα επιστρέφει την ρύθμιση της θερμοκρασίας
+    // This method should return the current temperature of the heater
     public double getTemperature() {
     }
 }
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,35 +1,45 @@
 /**
  * Αυτή η κλάση αναπαριστά ένα ορθογώνιο.
- *
+ * This class represents a rectangle.
  */
 public class Rectangle {
 
     /*
     O κατασκευαστής θα δέχεται δύο παραμέτρους. Η πρώτη
     παράμετρος θα αφορά το μήκος και η δεύτερη το πλάτος του ορθογωνίου.
+
+    The constructor should have 2 parameters. The first parameter
+    should be the length of the rectangle and the second one its width.
     */
 
-    /* Μέθοδος που θα επιστρέφει την περίμετρο του ορθογωνίου */
+    /* Μέθοδος που θα επιστρέφει την περίμετρο του ορθογωνίου
+     * This method should return the perimeter of the rectangle */
+
     public double getPerimeter() {
     }
 
-    /* Μέθοδος που θα επιστρέφει τo εμβαδό του ορθογωνίου */
+    /* Μέθοδος που θα επιστρέφει τo εμβαδό του ορθογωνίου
+     * This method should return the total area of the rectangle */
     public double getArea() {
     }
 
-    /* Μέθοδος που αλλάζει το μήκος του ορθογωνίου */
+    /* Μέθοδος που αλλάζει το μήκος του ορθογωνίου
+     * This method should change the length of the rectangle */
     public void setWidth(double x) {
     }
 
-    /* Μέθοδος που αλλάζει το πλάτος του ορθογωνίου */
+    /* Μέθοδος που αλλάζει το πλάτος του ορθογωνίου
+     * This method should change the width of the rectangle */
     public void setHeight(double x) {
     }
 
-    /* Μέθοδος που επιστρέφει το μήκος του ορθογωνίου */
+    /* Μέθοδος που επιστρέφει το μήκος του ορθογωνίου
+     * This method should return the length of the rectangle */
     public double getWidth() {
     }
 
-    /* Μέθοδος που επιστρέφει το πλάτος του ορθογωνίου */
+    /* Μέθοδος που επιστρέφει το πλάτος του ορθογωνίου
+     * This method should return the width of the rectangle*/
     public double getHeight() {
 
     }
