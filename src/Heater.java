c8961e21e1318f7859c32142037b25b53cacc868
Athanasios Lagopoulos <sakrifor@gmail.com>
2019-10-06 12:23:46 +0300

    Initial Commit

--- src/Heater.java
+++ src/Heater.java
@@ -10,7 +10,7 @@
  * Initially, this step is set to 5 degrees. The temperature should not exceed an upper
  * and a lower limit.
  */
-public class Heater {    
+public class Heater {
     /* 
     O κατασκευαστής θα δέχεται δύο παραμέτρους. Η πρώτη 
     παράμετρος θα αφορά στην ελάχιστη και η δεύτερη στην μέγιστη θερμοκρασία 
@@ -19,24 +19,45 @@ public class Heater {
     The constructor should have two parameters. The first is the minimum temperature
     and the second parameter is the maximum temperature which the heater can be set to.
     */
+    double min = 0;
+    double max = 0;
+    double currentTemperature = 15;
+    double step = 5;
+
+    public Heater(double min, double max) {
+        this.min = min;
+        this.max = max;
+
+    }
+
 
     // Η μέθοδος αυτή θα αυξάνει την θερμοκρασία σύμφωνα με το βήμα
     // This method increases the temperature using the step
     public void warmer() {
+        double temporaryTemp = this.currentTemperature + this.step;
+        if (temporaryTemp <= this.max) {
+            this.currentTemperature = temporaryTemp;
+        }
     }
 
     // Η μέθοδος αυτή θα μειώνει την θερμοκρασία σύμφωνα με το βήμα
     // This method decreases the temperature using the step
     public void colder() {
+        double temporaryTemp = this.currentTemperature - this.step;
+        if (temporaryTemp >= this.min) {
+            this.currentTemperature = temporaryTemp;
+        }
     }
 
     //Η μέθοδος αυτή θα αλλάζει το βήμα
     // This method changes the step
     public void setIncrement(double step) {
+        this.step = Math.abs(step);
     }
 
     // η μέθοδος αυτή θα επιστρέφει την ρύθμιση της θερμοκρασίας
     // This method should return the current temperature of the heater
     public double getTemperature() {
+        return this.currentTemperature;
     }
 }
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -6,41 +6,56 @@ public class Rectangle {
 
     /*
     O κατασκευαστής θα δέχεται δύο παραμέτρους. Η πρώτη
-    παράμετρος θα αφορά το μήκος και η δεύτερη το πλάτος του ορθογωνίου.
+    παράμετρος θα αφορά το πλάτος και η δεύτερη το ύψος του ορθογωνίου.
 
     The constructor should have 2 parameters. The first parameter
-    should be the length of the rectangle and the second one its width.
+    should be the width of the rectangle and the second one its height.
     */
+    double width = 0;
+    double height = 0;
+
+    public Rectangle(double width, double height) {
+        this.height = height;
+        this.width = width;
+    }
 
     /* Μέθοδος που θα επιστρέφει την περίμετρο του ορθογωνίου
      * This method should return the perimeter of the rectangle */
 
     public double getPerimeter() {
+        return this.width * 2 + this.height * 2;
     }
 
     /* Μέθοδος που θα επιστρέφει τo εμβαδό του ορθογωνίου
      * This method should return the total area of the rectangle */
     public double getArea() {
-    }
-
-    /* Μέθοδος που αλλάζει το μήκος του ορθογωνίου
-     * This method should change the length of the rectangle */
-    public void setWidth(double x) {
+        return this.width * this.height;
     }
 
     /* Μέθοδος που αλλάζει το πλάτος του ορθογωνίου
      * This method should change the width of the rectangle */
-    public void setHeight(double x) {
+    public void setWidth(double x) {
+        this.width = x;
+
     }
 
-    /* Μέθοδος που επιστρέφει το μήκος του ορθογωνίου
-     * This method should return the length of the rectangle */
-    public double getWidth() {
+    /* Μέθοδος που αλλάζει το ύψος του ορθογωνίου
+     * This method should change the height of the rectangle */
+    public void setHeight(double x) {
+        this.height = x;
+
     }
 
     /* Μέθοδος που επιστρέφει το πλάτος του ορθογωνίου
-     * This method should return the width of the rectangle*/
+     * This method should return the width of the rectangle */
+    public double getWidth() {
+        return this.width;
+    }
+
+    /* Μέθοδος που επιστρέφει το ύψος του ορθογωνίου
+     * This method should return the height of the rectangle*/
     public double getHeight() {
+        return this.height;
 
     }
 
